44bdfc8f9934e82ecf874110138cec30f9d6cfff
aftershock009 <aseprosadialawi@outlook.co.id>
2023-11-12 09:33:18 +0000

    Horizon:  Horizon wallpaper

--- config/version.mk
+++ config/version.mk
@@ -1,12 +1,37 @@
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ANDROID_VERSION := 14
+CUSTOMVERSION := X1.0
+CUSTOM_MOD_VERSION := ENDFIELD
+
+CUSTOM_BUILD_TYPE ?= UNOFFICIAL
+CUSTOM_MAINTAINER ?= UNKNOWN
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
 CUSTOM_DATE_DAY := $(shell date -u +%d)
 CUSTOM_DATE_HOUR := $(shell date -u +%H)
 CUSTOM_DATE_MINUTE := $(shell date -u +%M)
-CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
-CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)-$(CUSTOM_DATE_MINUTE)
+TARGET_PRODUCT_SHORT := $(subst CUSTOM_,,$(CUSTOM_BUILD))
+CUSTOM_VERSION := HorizonDroid-$(CUSTOMVERSION)-$(CUSTOM_MOD_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_MOD_VERSION :=$(ANDROID_VERSION)-$(CUSTOMVERSION)
+CUSTOM_DISPLAY_VERSION := HorizonDroid-$(CUSTOMVERSION)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_DISPLAY_BUILDTYPE := $(CUSTOM_BUILD_TYPE)
+CUSTOM_FINGERPRINT := HorizonDroid/$(CUSTOM_MOD_VERSION)/$(TARGET_PRODUCT_SHORT)/$(CUSTOM_BUILD_DATE)
 
-CUSTOM_PLATFORM_VERSION := 14.0-ENDFIELD-X1.0
-
-CUSTOM_VERSION := HorizonDroid_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
-CUSTOM_VERSION_PROP := fourteen
+# CUSTOM System Version
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.custom.version=$(CUSTOM_DISPLAY_VERSION) \
+  ro.custom.build.status=$(CUSTOM_BUILD_TYPE) \
+  ro.modversion=$(CUSTOM_MOD_VERSION) \
+  ro.custom.build.date=$(CUSTOM_BUILD_DATE) \
+  ro.custom.buildtype=$(CUSTOM_BUILD_TYPE) \
+  ro.custom.fingerprint=$(CUSTOM_FINGERPRINT) \
+  ro.custom.device=$(CUSTOM_BUILD) \
