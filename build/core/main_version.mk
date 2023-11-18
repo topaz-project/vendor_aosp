fb7ad8052cc80736e1d9907d1cb26a5d5ad46320
ryzenforce990 <ryzenforce990@gmail.com>
2023-11-18 18:33:21 +0000

    vendor: Add HorizonDroid Bootnimation

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,18 +3,3 @@ ifneq ($(BUILD_FINGERPRINT),)
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
-
-# HorizonDroid Platform Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.custom.build.date=$(BUILD_DATE) \
-    ro.custom.device=$(CUSTOM_BUILD) \
-    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
-    ro.custom.version=$(CUSTOM_VERSION) \
-    ro.modversion=$(CUSTOM_VERSION)
-
-# Updater
-ifeq ($(IS_OFFICIAL),true)
-    ADDITIONAL_SYSTEM_PROPERTIES  += \
-        net.horizondroid.build_type=ci \
-        net.horizondroid.version=$(CUSTOM_VERSION_PROP)
-endif
